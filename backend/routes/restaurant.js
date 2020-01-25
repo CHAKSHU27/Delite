9c3d6278a2231d80aa547c4fb3d9d8272eff2d33
Chakshu <chakshugoyal999@gmail.com>
2020-01-25 15:05:18 +0530

    changes

--- backend/routes/restaurant.js
+++ backend/routes/restaurant.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router=express.Router()
-const {restDetails}=require('../controllers/Restaurant')
-const {requireLogIn}=require('../controllers/userAuth')
+const {restDetails,allResDetails}=require('../controllers/Restaurant')
 const {restDetailsValidator}=require('../validator/restaurant')
  
+router.get('/all/restaurant-details',allResDetails)
 router.post('/restaurant-details',restDetailsValidator,restDetails);
 
 module.exports=router
